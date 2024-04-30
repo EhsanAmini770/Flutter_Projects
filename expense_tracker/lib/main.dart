5e154a4ecf8cdaa8507033febe0156de4a174f4b
Ehsan Amini <u201118076@samsun.edu.tr>
2024-04-30 17:09:47 +0300

    xylophone project

--- expense_tracker/lib/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:expense_tracker/expenses.dart';
-import 'package:flutter/material.dart';
-
-void main() {
-  runApp(const MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Expenses(),
-  ));
-}
